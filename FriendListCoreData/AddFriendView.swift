e509310bef69ad5656f381642e0198a9581901f6
NEW <new@NEWs-MacBook-Pro.local>
2022-06-23 11:46:32 -0600

    I removed the sample code and added a toolbar button to show addfriend screen

---
+++ FriendListCoreData/AddFriendView.swift
@@ -0,0 +1,56 @@
+//
+//  AddFriendView.swift
+//  FriendListCoreData
+//
+//  Created by NEW on 6/23/22.
+//
+
+
+import SwiftUI
+
+struct AddFriendView: View {
+    
+    @Environment(\.managedObjectContext) var moc
+    
+    @State private var name = ""
+    @State private var age = ""
+    @State private var vibe = ""
+    @State private var review = ""
+    @State private var rating = 3
+    
+    let vibes = ["Cool", "Chill", "No Vibe", "Mean", "Angry",]
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Name of friend", $name)
+                    
+                    Picker("Vibes", selection: $vibe) {
+                        ForEach(vibes, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                
+                Section {
+                    TextEditor(text: $review)
+                    
+                    Picker("Rate Friend", selection: $rating) {
+                        ForEach(0..<6) {
+                            Text(String($0))
+                        }
+                    }
+                } header : {
+                    Text("Rate your friend")
+                }
+            }
+        }
+    }
+}
+
+struct AddFriendView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddFriendView()
+    }
+}
--- FriendListCoreData/ContentView.swift
+++ FriendListCoreData/ContentView.swift
@@ -12,31 +12,52 @@ struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
     @FetchRequest(sortDescriptors: []) var friends: FetchedResults<Friend>
     
+    @State private var showingAddFriendView = false
+    
     var body: some View {
-        VStack {
-            List(friends) { friend in
-                Text(friend.name ?? "No friends")
-            }
-            
-            Button("Add") {
-                let firstNames = ["Beto", "Juan","Licuado"]
-                let lastNames = ["Reyes", "Nieto", "Soler"]
-                
-                let randomFirst = firstNames.randomElement()!
-                let randomLast = lastNames.randomElement()!
-                
-                let friend = Friend(context: moc)
-                friend.id = UUID()
-                friend.name = "\(randomFirst) \(randomLast)"
-                
-                try? moc.save()
-                
-            }
-
+        NavigationView {
+            Text("Number of friends: \(friends.count)")
+                .navigationTitle("Friends List")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button {
+                            showingAddFriendView.toggle()
+                        } label: {
+                            Label("Add Book", systemImage: "plus")
+                        }
+                    }
+                }
+                .sheet(isPresented: $showingAddFriendView) {
+                    AddFriend()
+                }
         }
-            .padding()
     }
 }
+
+//func exampleCode() {
+    //        VStack {
+    //            List(friends) { friend in
+    //                Text(friend.name ?? "No friends")
+    //            }
+    //
+    //            Button("Add") {
+    //                let firstNames = ["Beto", "Juan","Licuado"]
+    //                let lastNames = ["Reyes", "Nieto", "Soler"]
+    //
+    //                let randomFirst = firstNames.randomElement()!
+    //                let randomLast = lastNames.randomElement()!
+    //
+    //                let friend = Friend(context: moc)
+    //                friend.id = UUID()
+    //                friend.name = "\(randomFirst) \(randomLast)"
+    //
+    //                try? moc.save()
+    //
+    //            }
+    //
+    //        }
+    //            .padding()
+//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
